3af4a2a78717a3e2e4bce756e4c08fb9805c1772
Anton Jacobsson <ajacobsson98@gmail.com>
2020-06-24 22:42:41 +0200

    feat(settings): user can change language and currency

--- src/modules/settings/settings.ts
+++ src/modules/settings/settings.ts
@@ -2,6 +2,7 @@ import {I18N} from 'aurelia-i18n';
 import {inject} from 'aurelia-framework';
 import { CurrencyService } from 'services/currency-service';
 import { LanguageService } from 'services/language-service';
+import { observable } from 'aurelia-framework';
 @inject(I18N, CurrencyService, LanguageService)
 
 export class Settings {
@@ -9,24 +10,36 @@ export class Settings {
     private _currencyService: CurrencyService;
     private _languageService: LanguageService;
     
-    public translation: string;
-    public language: string;
-    public languageFromService: string;
-    public currency: string;
+    public currencies: string[] = ['USD', 'SEK'];
+    public languages: any[] = [{name: 'Svenska', locale: 'sv'}, {name: 'English', locale: 'en'} ];
+
+    @observable public selectedCurrency: string;
+    @observable public selectedLanguage: string;
 
   constructor(i18n: I18N, currencyService: CurrencyService, languageService: LanguageService) {
     this._i18n = i18n;
     this._currencyService = currencyService;
     this._languageService = languageService;
-
   }
 
   activate() {
-    this.translation = this._i18n.getLocale();
-    this.language = window.navigator.language;
-    this.languageFromService = this._languageService.getLanguageFromLocalStorage();
-    this.currency = this._currencyService.getCurrencyFromLocalStorage();
+    this.selectedLanguage = this._languageService.getLanguageFromLocalStorage();
+    this.selectedCurrency = this._currencyService.getCurrencyFromLocalStorage();
   }
 
+  selectedCurrencyChanged(newValue: string, oldValue: any) {
+    if(oldValue !== undefined) {
+      this._currencyService.setCurrencyToLocalStorage(newValue);
+    }
+    
+  }
+
+  selectedLanguageChanged(newValue: any, oldValue: any) {
+    if(oldValue !== undefined) {
+      this._languageService.setLanguageToLocalStorage(newValue);
+      window.location.reload();
+    }
+    
+  }
 
 }
